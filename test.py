44b3beba1a1be9fa70e1755f8e0aa8dd4eceb1a4
Rad099 <209367335+WesleyB12@users.noreply.github.com>
2025-12-14 14:14:06 -0600

    basic programming test

---
+++ test.py
@@ -0,0 +1,11 @@
+x = 1
+y = 2
+
+if (x > y):
+    print (x, " is greater than", y)
+    if (x < y):
+        print (x, " is less than", y)
+    else:
+        print (x, " is not less than", y)
+else:
+    print (x, " is not greater than", y)
