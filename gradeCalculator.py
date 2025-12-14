f225460fe3664070fa883b0d6fe83f61ab5167cc
Rad099 <209367335+WesleyB12@users.noreply.github.com>
2025-12-14 14:43:54 -0600

    grade calculating program

---
+++ gradeCalculator.py
@@ -0,0 +1,12 @@
+score = int(input("Enter your grade: "))
+
+if (score >= 90):
+    print("Your grade is an 'A'")
+elif (score >= 80):
+    print("Your grade is an 'B'")
+elif (score >= 70):
+    print("Your grade is an 'C'")
+elif (score >= 60):
+    print("Your grade is an 'D'")
+else:
+    print("Your grade is an 'F'")
